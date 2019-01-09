0d108da0b0b392fe5eaa7b59dcd2551358526692
qkj <598420290@qq.com>
2019-01-09 18:56:28 +0800

    修改检查方式：检查LAZ前3个字符
    增加用python请求下载laz文件（效果不行，可能被阻断？）

--- checkFail.py
+++ checkFail.py
@@ -1,45 +1,59 @@
 # -*- coding: utf-8 -*-  
 import os,shutil
 
+
 def getdirs(path, dirs):
+	#传入存储的list
 	for file in os.listdir(path):
 		file_path = os.path.join(path, file)
 		if os.path.isdir(file_path):
 			dirs.append(file_path)
 
 def getfiles(path, files):
+	#传入存储的list
 	for file in os.listdir(path):
 		file_path = os.path.join(path, file)
 		if not os.path.isdir(file_path):
 			files.append(file_path)
 
 data_dir = 'f:/laz_data/'
-fail_dir = 'f:/laz_data/fail/'
+fail_dir = 'f:/laz_data/fail'
 file_error = []
 dirs = []
 getdirs(data_dir, dirs)
+
 for i in range(len(dirs)):
+	if dirs[i] == fail_dir:
+		continue
 	print dirs[i]
+
 	files = []
 	getfiles(dirs[i], files)
 	# print len(files)
 	for j in range(len(files)):
 		fname,fext = os.path.splitext(files[j])
 		if fext == '.laz':
-			size = os.path.getsize(files[j])
-			if size <= 243:
+			read = open(files[j], 'r')
+			start_str = read.read(3)
+			read.close()
+
+			if start_str != 'LAS':
 				file_error.append(files[j])
+				# print start_str
+			# size = os.path.getsize(files[j])
+			# if size <= 243:
+			# 	file_error.append(files[j])
 
 print len(file_error)
+file_error = map(lambda x: x.replace('\\','/') + '\n', file_error)
+with open(data_dir + 'error.txt', 'w') as f:
+	f.writelines(file_error)
 
 if not os.path.exists(fail_dir):
 	os.mkdir(fail_dir)
 
 for i in range(len(file_error)):
-	target = fail_dir + ''
-	shutil.move(file_error[i], fail_dir)
+	target = fail_dir + '/'
+	# shutil.move(file_error[i], fail_dir)
 
 
-file_error = map(lambda x: x.replace('\\','/') + '\n', file_error)
-with open(data_dir + 'error.txt', 'w') as f:
-	f.writelines(file_error)
---
+++ downByCode.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-  
+import os
+
+import requests 
+import urllib
+import urllib2
+import datetime
+
+def getfiles(path, files):
+	#传入存储的list
+	for file in os.listdir(path):
+		file_path = os.path.join(path, file)
+		if not os.path.isdir(file_path):
+			files.append(file_path)
+
+
+print "Start downloading......"
+data_dir = 'f:/laz_data/'
+files = []
+getfiles(data_dir, files)
+
+bats = []
+for file in files:
+	fname,fext = os.path.splitext(file)
+	if fext == '.bat':
+		bats.append(file)
+# print bats
+
+bats = ['F:/laz_data/9.bat']
+ok = False
+for bat in bats:
+	print bat
+	file = os.path.basename(bat)
+	fname = file.split('.')[0]
+	with open(bat, 'r') as read:
+		num = 1
+		line = read.readline()
+		while line:
+			if num % 5 == 0:
+				# log.flush()
+				print num
+			strs = line.split(' ')
+			save_laz = strs[2]
+			url = strs[3]
+
+			# if url == 'http://na-c.entwine.io/nyc/ept-data/8-1-50-250.laz':
+			# 	ok = True
+			# if not ok:
+			# 	continue
+
+			t = str(datetime.datetime.now())
+			log = open(data_dir + 'log' + fname + '.txt','a')
+			log.write('Start: ' + t + '   ' + url)
+			log.close()
+
+			urllib.urlretrieve(url, save_laz)
+
+			# r = requests.get(url) 
+			# with open(save_laz, "wb") as code:
+			# 	code.write(r.content)
+
+			# f = urllib2.urlopen(url) 
+			# data = f.read() 
+			# with open(save_laz, "wb") as code: 
+			# 	code.write(data)
+
+			line = read.readline()
+
+
+
+
+
